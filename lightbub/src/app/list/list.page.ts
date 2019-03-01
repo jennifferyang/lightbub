6b988470dabc7ce2428ab3f9dba3a7f2b6553393
Jenny Yang <jenniffer_yang@icloud.com>
2019-03-01 15:45:03 -0500

    another mistake :-1:

--- lightbub/src/app/list/list.page.ts
+++ lightbub/src/app/list/list.page.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-list',
-  templateUrl: 'list.page.html',
-  styleUrls: ['list.page.scss']
+@ -7,26 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
   private selectedItem: any;
+  private icons = [
+    'flask',
+    'wifi',
+    'beer',
+    'football',
+    'basketball',
+    'paper-plane',
+    'american-football',
+    'boat',
+    'bluetooth',
+    'build'
+  ];
    private icons = [
      'bulb', 
      'information',
@@ -23,6 +30,12 @@ export class ListPage implements OnInit {
    ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
     this.items.push({
       title: 'Home',
       note: 'Your Lightbug Lives Here',
@@ -47,11 +60,3 @@ export class ListPage implements OnInit {
       //);
     }
   }
-
-  ngOnInit() {
-  }
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
-}
