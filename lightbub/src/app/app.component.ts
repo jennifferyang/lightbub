934a44918d2a860509ae7ac77353cf33a228c7b7
ayou19 <a.younkman19@gmail.com>
2019-03-07 19:51:19 -0500

    NEW TABS!

--- lightbub/src/app/app.component.ts
+++ lightbub/src/app/app.component.ts
@@ -16,9 +16,24 @@ export class AppComponent {
       icon: 'home'
     },
     {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
+      title: 'Badges',
+      url: '/badges',
+      icon: 'apps'
+    },
+    {
+      title: 'Rooms',
+      url: '/rooms',
+      icon: 'compass'
+    },
+    {
+      title: 'Energy Usage',
+      url: '/energy',
+      icon: 'stats'
+    },
+    {
+      title: 'Info',
+      url: '/info',
+      icon: 'information-circle-outline'
     }
   ];
 
